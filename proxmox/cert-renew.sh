e8eaf26b1174341b20b1f490d618dac2bb2225de
familyman-131 <familyman-131@users.noreply.github.com>
2019-10-09 17:04:28 +0300

    Create cert-renew.sh

---
+++ proxmox/cert-renew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+domain="example.com"
+
+# comment after first use
+certbot certonly --standalone --agree-tos -m user@email -d ${domain}
+
+/usr/bin/certbot renew >> /var/log/letsencrypt/letsencrypt.log
+rm -rf /etc/pve/local/pve-ssl.pem
+rm -rf /etc/pve/local/pve-ssl.key
+rm -rf /etc/pve/pve-root-ca.pem
+cp /etc/letsencrypt/live/${domain}/fullchain.pem  /etc/pve/local/pve-ssl.pem
+cp /etc/letsencrypt/live/${domain}/privkey.pem /etc/pve/local/pve-ssl.key
+cp /etc/letsencrypt/live/${domain}/chain.pem /etc/pve/pve-root-ca.pem
+service pveproxy restart
+service pvedaemon restart
